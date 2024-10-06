4bb1d7852b3b53f89549cef12da073dac4e59168
Daniel M Rocha _ Predator-i7 <danroch1913@gmail.com>
2024-10-06 14:31:59 -0300

    validação da entrada como data valida e alguns comentarios estilo javadoc

--- src/MoonFasePkg/App.java
+++ src/MoonFasePkg/App.java
@@ -2,10 +2,38 @@ package MoonFasePkg;
 
 import java.time.LocalDate;
 
+/**
+ * Aplicação que processa um objeto LocalDate e informa a fase lunar deste.
+ * @author Daniel Machado da Rocha
+ * @version 0.1
+ * @since 28/9/2024
+ */
 
 public class App implements I_MoonFase{
+    public static LocalDate dateRef;
 
-    public static Fase calculoDaFase(LocalDate dateRef){
+    /**
+     * Método que analiza se o LocalDate é valido, sendo valido a variável static dataRef é setada.
+     * @param day dia
+     * @param month mês
+     * @param year ano
+     * @return boleano com a definição se a data informada é ou não valida.
+     */
+    public static boolean setMoonDate(int day,int month, int year){
+        try{
+            dateRef = LocalDate.of(year,month,day);
+            return true;
+        }catch (Exception e){
+            return false;
+        }
+    }
+
+    /**
+     * Método que aplica o algoritmo matemático.
+     * @param dateRef Data para processamento.
+     * @return Resultado do processamento como parametro da função lua, que retorna o Enum da fase em questão.
+     */
+    private Fase calculoDaFase(LocalDate dateRef){
         LocalDate dataReference = dateRef;
 
         int day = dataReference.getDayOfMonth();
@@ -17,7 +45,12 @@ public class App implements I_MoonFase{
         return lua(algoritmoLunar);
     }
 
-    public static Fase lua(int algoLunar){
+    /**
+     * Método que seleciona o Enum conforme o resultado do algoritmo lunar.
+     * @param algoLunar Algoritmo proveniente da função calculoDaFase.
+     * @return O Enum com a fase lunar.
+     */
+    private Fase lua(int algoLunar){
         Fase result = null;
         if(algoLunar>=0 && algoLunar <=7){
             result = Fase.NOVA;
@@ -31,7 +64,12 @@ public class App implements I_MoonFase{
         return result;
     }
 
-    public static int yearPreparation( int year){
+    /**
+     * Método que verifica o ano, pois o algoritmo é diferente se o ano for menor que 2000.
+     * @param year ano
+     * @return Ano retificado para o processamento do algoritmo.
+     */
+    private int yearPreparation( int year){
 
         int auxAno = year;
 
